82c21900befff54cc7966f60a849b8121d953f5e
fyodorussia <fvdelows@mail.ru>
2019-04-27 19:08:05 +0300

    сделал вход(примерно)

--- Alisa/database_module.py
+++ Alisa/database_module.py
@@ -10,85 +10,17 @@ class DatabaseManager:
         self.connection = sqlite3.connect("data/alisa_users.db", isolation_level=None)
         cursor = self.connection.cursor()
         cursor.execute('PRAGMA foreign_key=1')
-        cursor.execute('CREATE TABLE IF NOT EXISTS users'
-                       '(user_id TEXT PRIMARY KEY NOT NULL, name TEXT, last_char TEXT,'
-                       'current_score INT DEFAULT 0, max_score INT DEFAULT 0)')
-        cursor.execute('CREATE TABLE IF NOT EXISTS words'
-                       '(user_id TEXT NOT NULL, word TEXT,'
-                       'FOREIGN KEY (user_id) REFERENCES users(user_id))')
+        cursor.execute('''CREATE TABLE IF NOT EXISTS users 
+                            (id INTEGER PRIMARY KEY AUTOINCREMENT, 
+                             user_id TEXT NOT NULL,
+                             user_name TEXT,
+                             password TEXT)''')
         cursor.close()
 
     def __del__(self):
         self.connection.close()
 
-    def add_user(self, user_id: str, name: str = '', last_char: str = '',
-                 current_score: int = 0, max_score: int = 0) -> bool:
-        cursor = self.connection.cursor()
-        try:
-            if not self.get_entry(user_id):
-                cursor.execute("""INSERT INTO users 
-                                VALUES(:user_id, :name, :last_char,
-                                :current_score, :max_score)""", {
-                    'user_id': user_id, 'name': name, 'last_char': last_char,
-                    'current_score': current_score, 'max_score': max_score
-                })
-                print('Пользователь {} добавлен.'.format(user_id))
-            else:
-                print('Пользователь {} уже существует!'.format(user_id))
-                cursor.close()
-                return False
-        except sqlite3.DatabaseError as error:
-            print('Error: ', error)
-            cursor.close()
-            return False
-        else:
-            cursor.close()
-            return True
-
-    def update(self, user_id: str, new_last_char: str,
-               current_score: int = 0, max_score: int = 0) -> bool:
-        cursor = self.connection.cursor()
-        try:
-            exist_entry = self.get_entry(user_id)
-            if not exist_entry:
-                print('Пользователь с номером {} не существует!'.format(user_id))
-                return False
-            else:
-                print(exist_entry, ' - exist entry;;;    branch else in update')
-                cursor.execute("""UPDATE users
-                                SET last_char = :last_char, 
-                                current_score = :current_score,
-                                max_score = :max_score 
-                                WHERE user_id == :user_id""",
-                               {
-                                   'user_id': user_id, 'last_char': new_last_char,
-                                   'current_score': current_score, 'max_score': max_score
-                               })
-        except sqlite3.DatabaseError as error:
-            print('Error: ', error)
-            cursor.close()
-            return False
-        else:
-            cursor.close()
-            return True
-
-    def add_word(self, user_id: str, new_word: str) -> bool:
-        cursor = self.connection.cursor()
-        try:
-            cursor.execute("""INSERT INTO words 
-                            VALUES(:user_id, :word)""", {
-                'user_id': user_id, 'word': new_word
-            })
-            print('Пользователю {} добавлено слово {}.'.format(user_id, new_word))
-        except sqlite3.DatabaseError as error:
-            print('Error: ', error)
-            cursor.close()
-            return False
-        else:
-            cursor.close()
-            return True
-
-    def get_entry(self, user_id: str = '', get_all: bool = False) -> list:
+    def get_users(self, user_id: str = '', get_all: bool = False)->list:
         cursor = self.connection.cursor()
         result = []
         try:
@@ -96,7 +28,7 @@ class DatabaseManager:
                 cursor.execute("""SELECT * FROM users""")
             else:
                 cursor.execute("""SELECT * FROM users
-                                WHERE user_id = :user_id""",
+                                        WHERE user_id = :user_id""",
                                {
                                    'user_id': user_id,
                                })
@@ -107,40 +39,14 @@ class DatabaseManager:
         cursor.close()
         return result
 
-    def get_words(self, user_id: str) -> list:
+    def get_individ(self, user_name: str = '', password: str = '')->bool:
         cursor = self.connection.cursor()
-        result = []
         try:
-            cursor.execute("""SELECT * FROM words
-                            WHERE user_id = :user_id""",
+            cursor.execute("""SELECT * FROM users
+                                                    WHERE user_name = :user_name, password = :password""",
                            {
-                               'user_id': user_id,
+                               'user_name': user_name, 'password': password
                            })
-        except sqlite3.DatabaseError as error:
-            print('Error: ', error)
-        else:
-            result = cursor.fetchall()
-        cursor.close()
-        return [elem[1] for elem in result]
-
-    def delete_user(self, user_id: str) -> bool:
-        cursor = self.connection.cursor()
-        try:
-            exist_entry = self.get_entry(user_id)
-            if not exist_entry:
-                print('Пользователь с номером {} не существует!'.format(user_id))
-                return False
-            else:
-                cursor.execute("""DELETE FROM words
-                                WHERE user_id = :user_id""",
-                               {
-                                   'user_id': user_id
-                               })
-                cursor.execute("""DELETE FROM users
-                                WHERE user_id = :user_id""",
-                               {
-                                   'user_id': user_id
-                               })
         except sqlite3.DatabaseError as error:
             print('Error: ', error)
             cursor.close()
@@ -149,8 +55,22 @@ class DatabaseManager:
             cursor.close()
             return True
 
-    def make_leaderboard(self, size_of_table: int = 10) -> list:
-        values_list = self.get_entry(get_all=True)  # [('1', 'Dima', 'a', 2, 3), ('2', 'Mila', 'т', 1, 5)]
-        result = sorted(values_list, key=lambda inner_list: inner_list[4], reverse=True)
-        size_of_board = len(result) if len(result) < size_of_table else size_of_table
-        return [(i+1, result[i][1], result[i][4]) for i in range(size_of_board)]
+    def add_user(self, user_id: str, user_name: str = '', password: str = '') -> bool:
+        cursor = self.connection.cursor()
+        try:
+            if not self.get_individ(user_name, password):
+                cursor.execute("""INSERT INTO users 
+                                VALUES(:user_id, :user_name, :password)""", {
+                    'user_id': user_id, 'user_name': user_name, 'password': password})
+                print('Регистрация пользователя {}{}.'.format(user_id, user_name))
+            else:
+                print('Произведен вход пользователя {}{}.'.format(user_id, user_name))
+                cursor.close()
+                return False
+        except sqlite3.DatabaseError as error:
+            print('Error: ', error)
+            cursor.close()
+            return False
+        else:
+            cursor.close()
+            return True
--- Alisa/main_function.py
+++ Alisa/main_function.py
@@ -8,44 +8,36 @@ def handle_dialog(request, response, user_storage, database):
     if not user_storage:
         user_storage = {"suggests": []}
     input_message = request.command.lower()
-    if request.is_new_session:
-        if not database.get_entry(request.user_id):
-            output_message = "Привет-привет. Не хочешь ли ты сыграть в слова? Если да, то просто назови своё имя!"
-        else:
-            output_message = "И снова здравствуй! Продолжим игру??"
-            user_storage = {'suggests': [
-                "Продолжить",
-                "Начать сначала"
-            ]}
+    if request.is_new_session or response.end_session:
+        output_message = "Здравствуйте, Вас приветствует Ваш коммуникатор Адель." \
+                         " Чтобы перейти к работе просто скажите мне свой логин и пароль через пробел."
+        if response.end_session == True:
+            user_storage = {'suggests': [x[2] for x in database.get_users(request.user_id)]
+                            }
         return message_return(response, user_storage, output_message)
 
-    if "помощь" in input_message or "что ты умеешь" in input_message:
-        output_message = "В этой игре тебе просто-напросто нужно говорить слово, начинающиеся с той же буквы, " \
-                         "что заканчивается предыдущее. Статистика отгаданных слов сохраняется, при начале новой игры" \
-                         " счет в данный момент обнуляется, а максимальный остается. Что ж, сыграем?"
+    if not request.is_new_session and response.end_session:
+        input_message.split(' ')
+        database.add_user(request.user_id, input_message[0], input_message[1])
+        output_message = "Добро пожаловать {}!".format(input_message[0])
         user_storage = {'suggests': [
-            "Продолжить",
-            "Начать сначала"
         ]}
+        response.end_session = False
         return message_return(response, user_storage, output_message)
 
-    if input_message in ['не хочется', 'в следующий раз', 'выход', "не хочу", 'выйти', 'стоп']:
-        output_message = "Окей, напиши мне что-нибудь, если захочешь снова поиграть)"
-        response.end_session = True
-        return message_return(response, user_storage, output_message)
-
-    if not database.get_entry(request.user_id):
-        database.add_user(request.user_id, input_message)
-        output_message = "Что ж, я тебя запомню. Начнем?"
+    if not database.get_users(request.user_id):
+        input_message.split(' ')
+        database.add_user(request.user_id, input_message[0], input_message[1])
+        output_message = "Добро пожаловать {}!".format(input_message[0])
         user_storage = {'suggests': [
-            "Давай"
         ]}
         return message_return(response, user_storage, output_message)
 
     if "таблица лидеров" in input_message or "лидеры" in input_message or "рейтинг" in input_message:
         leaders = database.make_leaderboard()
         output_message = "Имеющиеся на данный момент лидеры:\n{}\n" \
-            .format(",\n".join(["{}. {}, счет - {}".format(i[0], i[1].capitalize(), i[2]) for i in leaders]))
+            .format(",\n".join(
+            ["{}. {}, счет - {}".format(i[0], i[1].capitalize(), i[2]) for i in leaders]))
         current_user_stats = database.get_entry(request.user_id)[0]
         current_user_stats = (current_user_stats[1], current_user_stats[4], current_user_stats[3])
         if not any([True if current_user_stats[:2] == lead[1:] else False for lead in leaders]):
@@ -63,14 +55,19 @@ def handle_dialog(request, response, user_storage, database):
     entry = database.get_entry(request.user_id)[0]
     if "продолжить" in input_message or "давай" in input_message or "сначала" in input_message or (
             "хочу" in input_message and "не" in input_message) and not entry[2]:
-        chosen_word = choice_wrd(entry[2] if entry[2] else choice("айцукенгшщзхфывапролджэячсмитбю"),
-                                 database.get_words(request.user_id))
+        chosen_word = choice_wrd(
+            entry[2] if entry[2] else choice("айцукенгшщзхфывапролджэячсмитбю"),
+            database.get_words(request.user_id))
         if "сначала" in input_message:
-            database.update(request.user_id, chosen_word[-1] if chosen_word[-1] not in "ьъ" else chosen_word[-2],
-                            current_score=0, max_score=entry[3] if entry[3] > entry[4] else entry[4])
+            database.update(request.user_id,
+                            chosen_word[-1] if chosen_word[-1] not in "ьъ" else chosen_word[-2],
+                            current_score=0,
+                            max_score=entry[3] if entry[3] > entry[4] else entry[4])
         else:
-            database.update(request.user_id, chosen_word[-1] if chosen_word[-1] not in "ьъ" else chosen_word[-2],
-                            current_score=entry[3], max_score=entry[3] if entry[3] > entry[4] else entry[4])
+            database.update(request.user_id,
+                            chosen_word[-1] if chosen_word[-1] not in "ьъ" else chosen_word[-2],
+                            current_score=entry[3],
+                            max_score=entry[3] if entry[3] > entry[4] else entry[4])
         output_message = "Только запомни, учитываться будет только первое слово. Что ж, начнём! Внимание, слово " \
                          "- {}.".format(chosen_word)
         user_storage = {'suggests': []}
@@ -83,12 +80,15 @@ def handle_dialog(request, response, user_storage, database):
                 used_words = database.get_words(request.user_id)
                 if user_word[0] not in used_words:
                     used_words.append(user_word[0])
-                    chosen_word = choice_wrd(user_word[0][-1] if user_word[0][-1] not in "ьъ" else user_word[0][-2],
-                                             used_words)
+                    chosen_word = choice_wrd(
+                        user_word[0][-1] if user_word[0][-1] not in "ьъ" else user_word[0][-2],
+                        used_words)
                     output_message = "Правильно! Следующее слово - {}.".format(chosen_word)
                     database.update(request.user_id,
-                                    chosen_word[-1] if chosen_word[-1] not in "ьъ" else chosen_word[-2],
-                                    current_score=entry[3] + 1, max_score=entry[3] if entry[3] > entry[4] else entry[4])
+                                    chosen_word[-1] if chosen_word[-1] not in "ьъ" else chosen_word[
+                                        -2],
+                                    current_score=entry[3] + 1,
+                                    max_score=entry[3] if entry[3] > entry[4] else entry[4])
                     database.add_word(request.user_id, user_word[0])
                 else:
                     output_message = "Неправильно, это слово уже использовалось!"
