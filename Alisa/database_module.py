4927ea19afc215a90bba30a41002ede4b17b04c1
fyodorussia <fvdelows@mail.ru>
2019-04-28 14:35:48 +0300

    правка 3 один id-один пользователь

--- Alisa/database_module.py
+++ Alisa/database_module.py
@@ -10,11 +10,12 @@ class DatabaseManager:
         self.connection = sqlite3.connect("data/alisa_users.db", isolation_level=None)
         cursor = self.connection.cursor()
         cursor.execute('PRAGMA foreign_key=1')
-        cursor.execute('''CREATE TABLE IF NOT EXISTS users 
-                            (id INTEGER PRIMARY KEY AUTOINCREMENT, 
-                             user_id TEXT NOT NULL,
-                             user_name TEXT,
-                             password TEXT)''')
+        cursor.execute('''CREATE TABLE IF NOT EXISTS users
+                            (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                             user_id VARCHAR(128),
+                             user_name VARCHAR(128),
+                             password VARCHAR(128)),
+                             status INTEGER''')
         cursor.close()
 
     def __del__(self):
@@ -30,10 +31,10 @@ class DatabaseManager:
                 cursor.execute("""SELECT * FROM users
                                         WHERE user_id = :user_id""",
                                {
-                                   'user_id': user_id,
+                                   'user_id': user_id
                                })
         except sqlite3.DatabaseError as error:
-            print('Error: ', error)
+            print('Error: ', error, '1')
         else:
             result = cursor.fetchall()
         cursor.close()
@@ -42,31 +43,47 @@ class DatabaseManager:
     def get_individ(self, user_id: str, user_name: str = '', password: str = '') -> list:
         cursor = self.connection.cursor()
         try:
-            cursor.execute("""SELECT * FROM users
-                                WHERE user_id = :user_id, user_name = :user_name, password = :password""",
-                           {
-                               'user_id': user_id, 'user_name': user_name, 'password': password
-                           })
+            cursor.execute("""SELECT * FROM users WHERE user_id = ? AND user_name = ? AND password = ?""", (user_id, user_name, password))
         except sqlite3.DatabaseError as error:
-            print('Error: ', error)
+            print('Error: ', error, '2')
             result = [False, []]
         else:
-            result = [True].append(cursor.fetchall())
+            if len(cursor.fetchall())==1:
+                result = [True]+[cursor.fetchall()]
+            else:
+                result = [False, []]
+        print('   444   ', result)
         cursor.close()
         return result
 
     def add_user(self, user_id: str, user_name: str = '', password: str = '') -> bool:
         cursor = self.connection.cursor()
         try:
-            if not self.get_individ(user_name, password)[0]:
-                cursor.execute("""INSERT INTO users 
-                                VALUES(:user_id, :user_name, :password)""", {
-                    'user_id': user_id, 'user_name': user_name, 'password': password})
+            print('   444   ', self.get_individ(user_id, user_name, password)[0])
+            if not self.get_individ(user_id, user_name, password)[0]:
+                cursor.execute('''INSERT INTO users
+                          (user_id, user_name, password)
+                          VALUES (?,?,?)''',
+                       (user_id, user_name, password))
                 print('Регистрация пользователя {}{}.'.format(user_id, user_name))
             else:
                 print('Произведен вход пользователя {}{}.'.format(user_id, user_name))
                 cursor.close()
                 return False
+        except sqlite3.DatabaseError as error:
+            print('Error: ', error, '3')
+            cursor.close()
+            return False
+        else:
+            cursor.close()
+            return True
+
+    def uppdate_status(self, user_id: str, status: int, user_name: str = '', password: str = '') -> bool:
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute("""UPDATE users
+                                SET status = ?
+                                WHERE user_id = ? AND user_name = ? AND password = ?""", (status, user_id, user_name, password))
         except sqlite3.DatabaseError as error:
             print('Error: ', error)
             cursor.close()
--- Alisa/main_function.py
+++ Alisa/main_function.py
@@ -8,37 +8,35 @@ def handle_dialog(request, response, user_storage, database):
     if not user_storage:
         user_storage = {"suggests": []}
     input_message = request.command.lower()
-    if request.is_new_session or response.end_session:
+
+    if input_message in ['выйти']:
+        output_message = "Обращайтесь ещё!)"
+        user_storage = {'suggests': ['Помощь', 'Войти']}
+        database.uppdate_status()
+        return message_return(response, user_storage, output_message)
+
+    if len(request.command.split(' ')) == 2 and read_answers_data("data/status")['global_status'] == 'out':
+        input_message=request.command.split(' ')
+        if database.get_individ(request.user_id, input_message[0], input_message[1])[0]:
+            output_message = "Добро пожаловать {}!".format(input_message[0])
+            user_storage = {'suggests': [
+                'Друзья', 'Группы', 'Помощь', 'Выход'
+            ]}
+            return message_return(response, user_storage, output_message)
+        else:
+            database.add_user(request.user_id, input_message[0], input_message[1])
+            output_message = "Добро пожаловать {}!".format(input_message[0])
+            user_storage = {'suggests': [
+                'Друзья', 'Группы', 'Помощь', 'Выход'
+            ]}
+            return message_return(response, user_storage, output_message)
+
+    if request.is_new_session or input_message in ['войти','регистрация']:
         output_message = "Здравствуйте, Вас приветствует Ваш коммуникатор Адель." \
                          " Чтобы перейти к работе просто скажите мне свой логин и пароль через пробел."
-        if response.end_session:
-            user_storage = {'suggests': [x[2] for x in database.get_users(request.user_id)]
-                            }
+        user_storage = {'suggests': ['Помощь']}
         return message_return(response, user_storage, output_message)
-
-    if not database.get_users(request.user_id) or (not request.is_new_session and response.end_session):
-        input_message=request.command.split(' ')
-        database.add_user(request.user_id, input_message[0], input_message[1])
-        output_message = "Добро пожаловать {}!".format(input_message[0])
-        user_storage = {'suggests': [
-            'Друзья', 'Группы', 'Помощь', 'Выход'
-        ]}
-        if not request.is_new_session and response.end_session:
-            response.end_session = False
-            response._response_dict['response']['end_session'] = response.end_session
-        return message_return(response, user_storage, output_message)
-
-    entry = database.get_individ(request.user_id)[1]
-    print(entry)
-
-
-
-
 
     buttons, user_storage = get_suggests(user_storage)
-    return message_error(response, user_storage, [
-        "Я жду ответа!",
-        "Не очень поняла ответ, но я надеюсь, что это была смешная шутка",
-        "Я не настолько умная, как человеки, поэтому ответьте крайне линейно",
-        "Вау... ничего не поняла"
+    return message_error(response, user_storage, ['Простите за конфуз, я ещё в разработке'
     ])
